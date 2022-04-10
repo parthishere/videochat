092ca4b625bc85a11db83106ba4d71891e8d6381
parthishere <parthishere1234@gmail.com>
2022-04-10 14:54:09 +0530

    changes

--- videochat/settings.py
+++ videochat/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
